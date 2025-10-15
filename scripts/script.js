f728be2a4af4bafe7bc5b2beb8e4b846d8c4d21c
Saaamuel <samuelcosta233@gamil.com>
2025-10-15 13:56:26 -0300

    atualizando

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,17 @@
-function mostrarMenu(){
-    $('nav#nav-esquerda ul.menu-principal').css('display', 'flex');
-    $('nav#nav-esquerda ul.menu-principal').addClass('animate__animated animate__fadeInRight animate__slow');
+const menu = document.getElementById('menu');
+const menuX = document.getElementById('menuX');
+const menuPrincipal = document.querySelector('.menu-principal');
 
-    $('nav#nav-esquerda ul#icone-menu li#menu').css('display', 'none');
-    $('nav#nav-esquerda ul#icone-menu li#menuX').css('display', 'flex');        
-}
+// Abre menu
+menu.addEventListener('click', () => {
+    menuPrincipal.classList.add('active');
+    menu.style.display = 'none';
+    menuX.style.display = 'block';
+});
 
-function esconderMenu(){
-    $('nav#nav-esquerda ul.menu-principal').css('display', 'none');
-
-    $('nav#nav-esquerda ul#icone-menu li#menu').css('display', 'flex');
-    $('nav#nav-esquerda ul#icone-menu li#menuX').css('display', 'none');   
-}
-
-let controle = true;
-
-$('nav#nav-esquerda ul#icone-menu').click(function(){
-    if(controle = true){
-        mostrarMenu();
-        controle=false;
-    }else{
-        esconderMenu();
-        controle=true;    
-    }
+// Fecha menu
+menuX.addEventListener('click', () => {
+    menuPrincipal.classList.remove('active');
+    menu.style.display = 'block';
+    menuX.style.display = 'none';
 });
